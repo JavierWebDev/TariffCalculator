66f4faa8f086a8e607430e94702e8b80a5e0897e
JavierWebDev <javier.gl200605@gmail.com>
2025-08-03 17:04:43 -0500

    feat: Finish Project
    Finish the project, add the application layer and small fixes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,47 @@
 package org.example
 
+import org.example.domain.model.Journey
+import org.example.domain.model.PassangerType
+import org.example.domain.service.FareCalculatorApp
+
 fun main() {
-    println("Hello World!")
+    var isActive = true
+    do {
+        var journey = defineJourney()
+        var passangerType = definePassanger()
+
+        var fc = FareCalculatorApp()
+
+        var finalFare = fc.calculateFare(journey, passangerType)
+
+        println("[*] FINAL FARE: $finalFare")
+
+        println("[?] Calculate another fare? y/n")
+        var opc = readln()
+        when (opc) {
+            "y" -> isActive = true
+            "n" -> isActive = false
+            else -> isActive = false
+        }
+
+
+
+    } while (isActive == true)
+}
+
+fun defineJourney() : Journey {
+    println("[?] Origin: ")
+    var origin = readln()
+
+    println("[?] Destination:")
+    var destination = readln()
+
+    return Journey(origin, destination)
+}
+
+fun definePassanger() : PassangerType {
+    println("[?] Rider Type: (Adult, Senior, Kid)")
+    var passangerType = readln()
+
+    return PassangerType(passangerType)
 }
